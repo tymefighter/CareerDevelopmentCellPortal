22c2d2dcaab2a5131d2182664996749d9722d868
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-05-18 15:08:38 +0530

    students who have got an internship/job cannot apply for internships or jobs now

--- code/php/student_home.php
+++ code/php/student_home.php
@@ -1,4 +1,25 @@
 <?php
+
+    session_start();
+    if ($_SESSION['login_info'] == null) {
+	$line = file_get_contents("../login_info.txt") or die("Unable to open file!");
+	$lines = explode("\n", $line);
+	if (count($lines) < 4)
+	   die("Some login information are missing!");
+       	$_SESSION['server']   = $lines[0];
+       	$_SESSION['user']     = $lines[1];
+       	$_SESSION['pass']     = $lines[2];
+       	$_SESSION['database'] = $lines[3];
+       	$_SESSION['login_info'] = TRUE;
+    }
+    
+    $server	= $_SESSION['server'];
+    $user	= $_SESSION['user'];
+    $pass	= $_SESSION['pass'];
+    $database	= $_SESSION['database'];
+?>
+
+<?php
     session_start();
     if($_SESSION['logged_in'] == null || $_SESSION['logged_in'] == false) {
         exit('Cannot Be Accessed Without Logging In');
@@ -7,6 +28,41 @@
     if($_SESSION['user_type'] != 'student') {
         exit('This webpage cannot be accessed by a ' . $_SESSION['user_type']);
     }
+
+    if($_SESSION['roll_number'] == null) {
+        exit('Huge Error Occurred');
+    }
+
+    // Try to establish connection to cdc database
+    $db = new mysqli ($server, $user, $pass, $database);
+    // Connection error, hence place error in log file
+    $error_num = mysqli_connect_errno();
+    if($error_num) {
+        error_log("error conn(student_home.php):  " . $error_num . "\n", 3, '../log_dir/log_file');
+        exit('');
+    }
+
+    # Check if student has got an internship
+    $query = 'select roll_number from accept_internship where roll_number = ?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s', $_SESSION['roll_number']);
+    $stmt->execute();
+    $stmt->store_result();
+
+    $accept_internship = $stmt->num_rows > 0;
+
+    # Check if student has got a job
+    $query = 'select roll_number from accept_job where roll_number = ?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s', $_SESSION['roll_number']);
+    $stmt->execute();
+    $stmt->store_result();
+
+    $accept_job = $stmt->num_rows > 0;
+
+    # Student cannot have both job and internship at a time
+    if($accept_intership && $accept_job)
+        exit('Huge Error Occurred');
 ?>
 
 <html>
@@ -48,8 +104,14 @@
             <br>
             <a href="../php/student_resume.php">My Resume</a>
             <br>
-            <a href="../php/student_applications.php">Applications</a>
-            <br>
+            <?php
+                if($accept_internship)
+                    echo '<a href="../php/student_internship.php">My Internship</a> <br>';
+                else if($accept_job)
+                    echo '<a href="../php/student_job.php">My Job</a> <br>';
+                else
+                    echo '<a href="../php/student_applications.php">Applications</a> <br>';
+            ?>
             <a href="../php/student_verification.php">Verification</a>
         </div>
 
@@ -62,9 +124,14 @@
                 ?>
             </div>
             <br><br>
-            <a class="main_link" href="../php/student_browse_internships.php">Browse Internships</a>
-            <br><br>
-            <a class="main_link" href="../php/student_browse_jobs.php">Browse Jobs</a>
+            <?php
+                if($accept_internship == false && $accept_job == false)
+                    echo '
+                            <a class="main_link" href="../php/student_browse_internships.php">Browse Internships</a>
+                            <br><br>
+                            <a class="main_link" href="../php/student_browse_jobs.php">Browse Jobs</a>
+                        ';
+            ?>
         </div>
    
 </body>
