1fd8712711f49a464f979dfc03562daa387914fc
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2020-06-06 01:06:51 +0530

    header file included

--- code/php/student_home.php
+++ code/php/student_home.php
@@ -76,39 +76,11 @@
         <link rel="stylesheet" href="../css_files/common.css">
         <link rel="stylesheet" href="../css_files/common_home.css">
         <script src="../javascript/validate.js"></script>
-        <script>
-        	function logout_confirm() {
-				if (confirm('Confirm logout ?'))
-					return true;
-				return false;
-			}
-		</script>
-    </head>
-    <body>
-        <ul class="nav">
-            <li class="nav"><a href='../php/home.php' class="nav">Home</a></li>
-            <li class="nav"><a href='https://iitpkd.ac.in' class="nav">IIT Palakkad</a></li>
-            <li class="nav"><a href="../php/companies.php" class="nav">Companies</a></li>
-            <li class="nav"><a href="../php/projects.php" class="nav">Projects</a></li>
-            <li class="nav"><a href="../php/research.php" class="nav">Research</a></li>
-            <li class="nav"><a href="../php/news.php" class="nav">News</a></li>
-            <?php
-                if($_SESSION['logged_in'] != null && $_SESSION['logged_in'] == true) {
-                    echo '<li class="nav"><a href="../php/logout.php" class="nav" onclick="return logout_confirm();">Logout</a></li>';
-                }
-                else {
-                    echo '<li class="nav"><a href="../php/login.php" class="nav">Login</a></li>';
-                    echo '<li class="nav"><a href="../php/register.php" class="nav">Register</a></li>';
-                }
-            ?>
-            <li id="nav_button">
-                <div class="cont" onclick="clickMenuButton(this)">
-                    <div class="bar1"></div>
-                    <div class="bar2"></div>
-                    <div class="bar3"></div>
-                </div>
-            </li>
-        </ul>
+</head>
+<body>
+        <?php 
+        	include('header.php');
+        ?>
 
         <div class="sidenav">
             <br>
